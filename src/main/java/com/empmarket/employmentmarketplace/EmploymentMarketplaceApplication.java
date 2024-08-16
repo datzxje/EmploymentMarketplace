6b75ce6e08d6de2acbdfc2b66496dc31a40a3b88
datzxje <tudabest12@gmail.com>
2024-08-16 10:52:08 +0700

    Initial Commit

---
+++ src/main/java/com/empmarket/employmentmarketplace/EmploymentMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.empmarket.employmentmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploymentMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmploymentMarketplaceApplication.class, args);
+    }
+
+}
